601c0fb365b8416b92901620361f23130e2cebc2
drtrungnguyen0306 <drtrungnguyen0306@gmail.com>
2017-02-22 16:42:49 +0700

    ref task/task-init-project
    ref task/task-main-activity

---
+++ app/src/main/java/com/fragmgia/photoeditor/util/ConstantManager.java
@@ -0,0 +1,8 @@
+package com.fragmgia.photoeditor.util;
+
+/**
+ * Created by trungnguyens93gmail.com on 2/23/17.
+ */
+public class ConstantManager {
+    public static final int LONG_TIME_THREAD = 3000;
+}
